8d7eae1f893e33b1801f1b4e259eccdafd4f0a72
Xuan4781 <123594280+Xuan4781@users.noreply.github.com>
2026-05-03 13:37:40 -0400

    add scene management and trust logic

--- Assets/Scripts/Core/GameManager.cs
+++ Assets/Scripts/Core/GameManager.cs
@@ -35,5 +35,82 @@ namespace DragonSeal.Core
             private set => _trustRating = Mathf.Clamp(value, 0, 100);
         }
 
+        private const int SCENE_MAINMENU = 0;
+        private const int SCENE_GAME = 1;
+        private const int SCENE_DAYEND = 2;
+        private const int SCENE_GAMEOVER = 3;
+        private const int SCENE_ENDING = 4;
+
+        private void Awake()
+        {
+            if (Instance != null && Instance != this)
+            {
+                Destroy(gameObject);
+                return;
+            }
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+
+        private void Start()
+        {
+            CurrentState = GameState.MainMenu;
+        }
+
+ 
+        public void StartNewGame()
+        {
+            _dayNumber = 1;
+            _trustRating = 100;
+            CurrentState = GameState.Playing;
+            SceneManager.LoadScene(SCENE_GAME);
+        }
+
+        public void EndDay()
+        {
+            CurrentState = GameState.DayEnd;
+            SceneManager.LoadScene(SCENE_DAYEND);
+        }
+
+        public void StartNextDay()
+        {
+            DayNumber++;
+            if (DayNumber > 3)
+            {
+                CurrentState = GameState.Ending;
+                SceneManager.LoadScene(SCENE_ENDING);
+            }
+            else
+            {
+                CurrentState = GameState.Playing;
+                SceneManager.LoadScene(SCENE_GAME);
+            }
+        }
+
+        public void TriggerGameOver()
+        {
+            CurrentState = GameState.GameOver;
+            SceneManager.LoadScene(SCENE_GAMEOVER);
+        }
+
+        public void RestartGame()
+        {
+            StartNewGame();
+        }
+
+        public void GoToMainMenu()
+        {
+            CurrentState = GameState.MainMenu;
+            SceneManager.LoadScene(SCENE_MAINMENU);
+        }
+
+        public void ModifyTrust(int amount)
+        {
+            TrustRating += amount;
+            Debug.Log($"Trust: {TrustRating}");
+
+            if (TrustRating <= 0)
+                TriggerGameOver();
+        }
     }
 }
