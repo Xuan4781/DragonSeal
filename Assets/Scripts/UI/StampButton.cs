ce3a2524fb1540ead54b50980db9f508e2659b05
Xuan4781 <123594280+Xuan4781@users.noreply.github.com>
2026-05-12 01:54:22 -0400

    update cover and add button ui, changed approvereject button color logic

--- Assets/Scripts/UI/StampButton.cs
+++ Assets/Scripts/UI/StampButton.cs
@@ -9,24 +9,19 @@ namespace DragonSeal.UI
     public class StampButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
     {
         [Header("Visuals")]
-        [SerializeField] private Color normalColor;
-        [SerializeField] private Color pressedColor;
         [SerializeField] private float pressOffset = 8f;
 
         [Header("Decision")]
         [SerializeField] private InspectionManager.StampDecision decision;
 
-        private Image _image;
         private RectTransform _rectTransform;
         private Vector2 _originalPosition;
         private bool _isPressed = false;
 
         private void Awake()
         {
-            _image = GetComponent<Image>();
             _rectTransform = GetComponent<RectTransform>();
             _originalPosition = _rectTransform.anchoredPosition;
-            _image.color = normalColor;
         }
 
         public void OnPointerDown(PointerEventData eventData)
@@ -35,15 +30,12 @@ namespace DragonSeal.UI
             _isPressed = true;
 
             _rectTransform.anchoredPosition = _originalPosition - new Vector2(0, pressOffset);
-            _image.color = pressedColor;
-
             StartCoroutine(StampSequence());
         }
 
         public void OnPointerUp(PointerEventData eventData)
         {
             _rectTransform.anchoredPosition = _originalPosition;
-            _image.color = normalColor;
         }
 
         private IEnumerator StampSequence()
